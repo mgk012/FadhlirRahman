28f1b143e052dd67ecced37be7808238e6276484
mgk012 <43450613+mgk012@users.noreply.github.com>
2022-04-22 22:20:43 +0700

    Create .py

---
+++ .py
@@ -0,0 +1,31 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="fadhlir" : print("akses diterima!!")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="fadhlir":
+# 	print("kamu sempurna!")
+# 	print("kamu juga rupawan!")
+# print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama=="zaldy":
+	print("hai fadhlir, akses diterima!!!")
+else:
+	print("kamu bukan fadhlir, akses ditolak! :(")
+
+print("akhir dari program")
